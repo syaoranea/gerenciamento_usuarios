c223358e29e7b34c8753895e6252b479e37c76cd
syaoranea <eduardo_rocha87@hotmail.com>
2021-12-08 12:49:15 -0300

    atualização

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,18 @@ class UserController {
         this.formEl = document.getElementById(formId);
         this.tableEl = document.getElementById(tableId);
 
-        this.onSubmit()
+        this.onSubmit();
+        this.onEdit();
+
+    }
+
+    onEdit(){
+
+        document.querySelector("#box-user-update .btn-cancel").addEventListener("click", e => {
+
+            this.showPanelCreate();
+
+        });
 
     }
 
@@ -15,8 +26,69 @@ class UserController {
 
             event.preventDefault();
 
-            this.addLine(this.getValues())
+            let btn = this.formEl.querySelector("[type=submit]");
+
+            btn.disabled = true;
+
+            let values = this.getValues();
+
+            if (!values) return false;
+
+            this.getPhoto().then(
+                (content) => {
+
+                    values.photo = content;
+
+                    this.addLine(values);
+
+                    this.formEl.reset();
+
+                    btn.disabled = false;
+
+                }, 
+                (e) => {
+                    console.error(e)
+                }
+            );
         
+        });
+
+    }
+
+    getPhoto(){
+
+        return new Promise((resolve, reject) => {
+
+            let fileReader = new FileReader();
+
+            let elements = [...this.formEl.elements].filter(item => {
+
+                if (item.name === 'photo') {
+                    return item;
+                }
+
+            });
+
+            let file = elements[0].files[0];
+
+            fileReader.onload = () => {
+
+                resolve(fileReader.result);
+
+            };
+
+            fileReader.onerror = (e) => {
+
+                reject(e);
+
+            };
+
+            if(file) {
+                fileReader.readAsDataURL(file);
+            } else {
+                resolve('dist/img/boxed-bg.jpg');
+            }
+
         });
 
     }
@@ -24,8 +96,16 @@ class UserController {
     getValues(){
 
         let user = {};
+        let isValid = true;
 
         [...this.formEl.elements].forEach(function(field, index){
+
+            if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value) {
+
+                field.parentElement.classList.add("has-error");
+                isValid = false
+
+            }
 
             if (field.name === "gender") {
     
@@ -33,6 +113,10 @@ class UserController {
                     user[field.name] = field.value
                 }
     
+            } else if(field.name == "admin") {
+
+                user[field.name] = field.checked;
+
             } else {
     
                 user[field.name] = field.value
@@ -40,6 +124,10 @@ class UserController {
             }
     
         });
+
+        if (!isValid) {
+            return false;
+        }
     
         return new User(
             user.name, 
@@ -53,25 +141,70 @@ class UserController {
         );
 
     }
-
     
     addLine(dataUser) {
 
-        this.tableEl.innerHTML = `
+        let tr = document.createElement('tr');
+
+        tr.dataset.user = JSON.stringify(dataUser);
+
+        tr.innerHTML = `
             <tr>
-                <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                <td><img src=${dataUser.photo} class="img-circle img-sm"></td>
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
-                <td>${dataUser.admin}</td>
-                <td>${dataUser.birth}</td>
+                <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
+                <td>${Utils.dateFormat(dataUser.register)}</td>
                 <td>
-                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                    <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
                     <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                 </td>
             </tr>
         `;
 
+        tr.querySelector(".btn-edit").addEventListener("click", e => {
+
+            console.log(JSON.parse(tr.dataset.user));
+            this.showPanelUpdate();
+
+        });
+
+        this.tableEl.appendChild(tr);
+
+        this.updateCount();
+
     }
 
+    showPanelCreate(){
 
+        document.querySelector("#box-user-create").style.display = "block";
+        document.querySelector("#box-user-update").style.display = "none";
+
+    }
+
+    showPanelUpdate(){
+
+        document.querySelector("#box-user-create").style.display = "none";
+        document.querySelector("#box-user-update").style.display = "block";
+
+    }
+
+    updateCount(){
+
+        let numberUsers = 0;
+        let numberAdmin = 0;
+
+        [...this.tableEl.children].forEach(tr => {
+
+            numberUsers++;
+
+            let user = JSON.parse(tr.dataset.user);
+
+            if (user._admin) numberAdmin++;
+        })
+
+        document.querySelector("#number-users").innerHTML = numberUsers;
+        document.querySelector("#number-users-admin").innerHTML = numberAdmin;
+
+    }
 }
